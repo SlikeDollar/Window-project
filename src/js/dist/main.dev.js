070ff5d7a1c6a3e8a015d8a2e77bfa856c3ca978
Igor Krasilnikov <mibonbon73@gmail.com>
2022-05-27 18:09:55 +0300

    timer added

--- src/js/dist/main.dev.js
+++ src/js/dist/main.dev.js
@@ -10,6 +10,8 @@ var _forms = _interopRequireDefault(require("./modules/forms"));
 
 var _changeModalState = _interopRequireDefault(require("./modules/changeModalState"));
 
+var _timer = _interopRequireDefault(require("./modules/timer"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -19,10 +21,12 @@ window.addEventListener("DOMContentLoaded", function () {
     form: "0",
     type: "tree"
   };
+  var deadline = '2022-07-18';
   (0, _changeModalState["default"])(modalState);
   (0, _modals["default"])(modalState);
   (0, _tabs["default"])('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
   (0, _tabs["default"])('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   (0, _tabs["default"])('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   (0, _forms["default"])(modalState);
+  (0, _timer["default"])('.container1', deadline);
 });
--- src/js/modules/dist/timer.dev.js
+++ src/js/modules/dist/timer.dev.js
@@ -5,7 +5,60 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var timer = function timer() {};
+var timer = function timer(id, deadline) {
+  var addZero = function addZero(num) {
+    if (num < 10) {
+      num = "0".concat(num);
+      return num;
+    } else {
+      return num;
+    }
+  };
+
+  var getTimeRemaining = function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor(t / 1000 % 60),
+        minutes = Math.floor(t / 1000 / 60 % 60),
+        hours = Math.floor(t / 1000 / 60 / 60 % 24),
+        days = Math.floor(t / 1000 / 60 / 60 / 24);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  };
+
+  var setClock = function setClock(selector, endtime) {
+    var timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(function () {
+      updateClock();
+    }, 1000);
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+      days.textContent = addZero(t.days);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total < 0) {
+        days.textContent = '0';
+        hours.textContent = '0';
+        minutes.textContent = '0';
+        seconds.textContent = '0';
+        clearInterval(timeInterval);
+      }
+    }
+  };
+
+  setClock(id, deadline);
+};
 
 var _default = timer;
 exports["default"] = _default;
